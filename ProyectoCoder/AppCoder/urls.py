1e67bbe592e4235c73ad16ff4ce6b70e296e0af3
Harold Merino <harold.merino@usach.cl>
2023-11-05 22:17:17 -0300

    Static_clase_10

--- ProyectoCoder/AppCoder/urls.py
+++ ProyectoCoder/AppCoder/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from AppCoder.views import *
 urlpatterns = [
-   
+    path('', inicio),
+    path('cursos/', cursos),
+    path('profesores/', profesores, name="profe"),
+    path('estudiantes/', estudiantes, name="estudiantes"),
+    path('entregables/', entregables),
+    path('curso/',curso,name='curso'),
 ]
--- ProyectoCoder/AppCoder/views.py
+++ ProyectoCoder/AppCoder/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
-from Appcoder.models import *
+from AppCoder.models import Curso, Estudiante
+from django.http import HttpResponse
+
 # Create your views here.
+def curso(self):
+    curso = Curso(nombre="Python", camada=55555)
+    curso.save()
+    documentoDeTexto = f"----->Curso {curso.nombre} Camada {curso.camada}" 
+    return HttpResponse(documentoDeTexto)
+
+def inicio(request):
+    return render(request, "AppCoder/index.html")
+
+def cursos(request):
+    return HttpResponse('vista cursos')
+
+def profesores(request):
+    return HttpResponse('vista profesores')
+
+def estudiantes(request):
+    return render(request, "AppCoder/estudiantes.html")
+
+def entregables(request):
+    return HttpResponse('vista entregables')
